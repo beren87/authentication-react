5b163a4ced6ec209785ff8074617d8626aff3c61
Berenger FERGUENIS <berenger@MacBook-Pro-de-Berenger.local>
2024-01-10 15:11:07 +0100

    Step 2 : Creating a Token API

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,29 @@
-import React, { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import './Login/Login.css'
-import Dashboard from './Dashboard/Dashboard'
-import Login from './Login/Login'
-import Preferences from './Preferences/Preferences'
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import './Login/Login.css';
+import Dashboard from './Dashboard/Dashboard';
+import Login from './Login/Login';
+import Preferences from './Preferences/Preferences';
 
 function App() {
   const [token, setToken] = useState();
 
-  if(!token){
-    return <Login setToken={setToken} />
+  if (!token) {
+    return <Login setToken={setToken} />;
   }
 
   return (
-    <div className='wrapper'>
+    <div className="wrapper">
       <h1>Application</h1>
       <BrowserRouter>
         <Routes>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/preferences">
-            <Preferences />
-          </Route>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/preferences" element={<Preferences />} />
         </Routes>
       </BrowserRouter>
-      </div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
