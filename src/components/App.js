70bb7bb736ebbfc9f845b5bf85ea7a2c7835b56a
Berenger FERGUENIS <berenger@MacBook-Pro-de-Berenger.local>
2024-01-12 10:30:04 +0100

    STEP 3 : Storing a User Token with sessionStorage & localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import './App.css';
 import './Login/Login.css';
 import Dashboard from './Dashboard/Dashboard';
 import Login from './Login/Login';
 import Preferences from './Preferences/Preferences';
+import userToken from './useToken';
 
 function App() {
-  const [token, setToken] = useState();
+
+  const { token, setToken } = userToken()
 
   if (!token) {
     return <Login setToken={setToken} />;
